b67f9c96093596a17dfbb632be860027d837f6cf
Miguel Soto <sistemas@rgd.com.co>
2024-08-13 21:22:55 -0500

    se añade método __str__ a ambos modelos para devolver el texto de ambos campos, y dar un valor representativo al objeto

--- polls/models.py
+++ polls/models.py
@@ -3,11 +3,17 @@ from django.db import models
 # Create your models here.
 # Entidad que representa la pregunta de la encuesta
 class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("fecha publicación")
 
 # Entidad que representa la respuesta o elección del votante
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
