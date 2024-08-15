7dc5c3acd904b1d080ed55cc2f09888f63338457
Miguel Soto <sistemas@rgd.com.co>
2024-08-15 08:01:29 -0500

    se registra la aplicación polls en el sitio administrativo

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'es-CO'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'CO'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
