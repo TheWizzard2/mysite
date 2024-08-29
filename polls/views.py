05af38b3e1ce3258cef11442229cda9283629f1a
Miguel Soto <sistemas@rgd.com.co>
2024-08-29 12:05:30 -0500

    se cambian las vistas a vistas genericas de django

--- polls/views.py
+++ polls/views.py
@@ -4,25 +4,26 @@ from django.template import loader
 from django.urls import reverse
 from polls.models import Choice, Question
 from django.db.models import F
+from django.views import generic
 
-def index(request):
-    # Capturar los datos desde el modelo objetos
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-    # El contexto es un diccionario que asigna nombres 
-    # de variables de plantilla a objetos de Python.
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
