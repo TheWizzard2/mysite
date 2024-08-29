8076816f274531fe9ba532c4b632b7c027393c6a
Miguel Soto <sistemas@rgd.com.co>
2024-08-29 11:45:23 -0500

    se añade al admin el modelo de Choices

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Se registra la aplicación en el sitio administrativo
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
